25f663eeea8db26bedb3e8c1584feaddd1b75c0e
joao.fernandes <joao.fernandes@blip.pt>
2016-05-12 15:55:46 +0100

    Add sidebar and header components

--- src/app/caius.component.ts
+++ src/app/caius.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   moduleId: module.id,
   selector: 'caius-app',
   templateUrl: 'caius.component.html',
-  styleUrls: ['caius.component.css']
+  styleUrls: ['caius.component.css'],
+  directives: [SidebarComponent, HeaderComponent]
 })
 export class CaiusAppComponent {
   title = 'caius works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -29,6 +29,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/sidebar',
+  'app/header',
   /** @cli-barrel */
 ];
 
